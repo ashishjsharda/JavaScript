dc820049eed09e46c5b5bb34100814b328263c3a
Ashish S <ashishjsharda@gmail.com>
2019-05-22 07:00:37 -0400

    Array Operations in JS
    Array Operations in JS

---
+++ arrays.js
@@ -0,0 +1,11 @@
+/**
+ * Array Operations in JS
+ */
+
+ let fruits=['Apple','Orange','Pear']
+         alert(fruits.length)
+         alert(fruits.pop())
+         alert(fruits)
+         //Push element 
+         fruits.push("Wwatermelon")
+         alert(fruits)
