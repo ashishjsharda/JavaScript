ede2f3bcfdc90d98c1cbcd52ba03776e085c9271
Ashish S <ashishjsharda@gmail.com>
2020-02-11 06:46:47 -0500

    Create arrays2.js

---
+++ arrays2.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Using Live Protocol</title>
+    
+    <script >
+        //Using Arrays
+       let arr=[1,2,3]
+       alert(arr[1])
+    </script>
+</head>
+<body>
+    
+</body>
+</html>
