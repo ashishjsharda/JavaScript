a9ea5b3dffbf66cf65f82601106386d72dc2495d
Ashish S <ashishjsharda@gmail.com>
2020-04-30 07:33:56 -0400

    Create usingstrings.js

---
+++ usingstrings.js
@@ -0,0 +1,11 @@
+let str="Hello";
+console.log(str[0]);
+console.log(str.length);
+for(let char of str)
+{
+    console.log(char);
+}
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.startsWith("He"));
+console.log(str.endsWith("lo"));
