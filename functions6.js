b02383f3b17f9fc6e14f7780e1e6edb99d806a5d
Ashish S <ashishjsharda@gmail.com>
2020-09-27 10:49:32 -0400

    Create functions6.js

---
+++ functions6.js
@@ -0,0 +1,11 @@
+let student={
+    name:"Jonathan Korn",
+    std:"XII",
+    major:"Science",
+
+    display(){
+        console.log("Student's Name is " +this.name + " Std is " +this.std + " Major is " + this.major)
+    }
+}
+
+student.display()
