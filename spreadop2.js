ed115d475c237c6be719204180b202713c181edb
Ashish S <ashishjsharda@gmail.com>
2020-11-12 17:23:32 -0500

    Create spreadop2.js

---
+++ spreadop2.js
@@ -0,0 +1,6 @@
+const hobbies=["Programming","Biking"]
+const coppiedArray=hobbies.slice();
+console.log(coppiedArray)
+//Using Spread Operator
+const coppiedArray2=[...hobbies]
+console.log(coppiedArray2)
